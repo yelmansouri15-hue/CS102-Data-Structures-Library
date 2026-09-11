41cf42ef8b341042329fd57cef50e01c65d2f5a3
Yasser <yelmansouri15@gmail.com>
2026-09-11 16:38:37 +0200

    Dynamic Array added

---
+++ Data_Structures_Library/src/cs102_2/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Hello from cs102-2!")
---
+++ Data_Structures_Library/src/data_structures/array_list.py
@@ -0,0 +1,40 @@
+import numpy
+
+class ArrayList:
+    def __init__(self):
+        self.a = numpy.empty(1, dtype=object)
+        self.n = 0
+
+    def error(self, i):
+        if i < 0 or i >= self.n : raise IndexError
+
+    def get(self, i):
+        self.error(i)
+        return self.a[i]
+
+    def set(self, i, x):
+        self.error(i)
+        y = self.a[i]
+        self.a[i] = x
+        return y
+
+    def add(self, i, x):
+        if i < 0 or i >= self.n : raise IndexError
+        if self.n == len(self.a) : self.resize()
+        self.a[i + 1:self.n + 1] = self.a[i:self.n]
+        self.a[i] = x
+        self.n += 1
+
+    def remove(self, i):
+        self.error(i)
+        x = self.a[i]
+        self.a[i:self.n - 1] = self.a[i + 1:self.n]
+        self.n -= 1
+        if len(self.a) >= 3*self.n : self.resize()
+        return x
+
+    def resize(self):
+        b = numpy.empty(max(2*self.n, 1), dtype=object)
+        b[:self.n] = self.a[:self.n]
+        self.a = b
+    
