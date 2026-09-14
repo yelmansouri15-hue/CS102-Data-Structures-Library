6ee2189eb4f4a2af6546469211c199359287596e
Yasser <yelmansouri15@gmail.com>
2026-09-14 21:34:01 +0200

    Stack operations (LIFO) added

--- Data_Structures_Library/src/data_structures/array_list.py
+++ Data_Structures_Library/src/data_structures/array_list.py
@@ -8,26 +8,31 @@ class ArrayList:
     def clear(self):
         self.__init__()
 
+    # Handles invalid index
     def error(self, i):
         if i < 0 or i >= self.n : raise IndexError
 
+    # Time complexity: O(1) [base adress + i * size element]
     def get(self, i):
         self.error(i)
         return self.a[i]
 
+    # Time complexity: O(1)
     def set(self, i, x):
         self.error(i)
-        y = self.a[i]
-        self.a[i] = x
+        y = self.a[i] # Get is O(1)
+        self.a[i] = x # O(1) + O(1) = O(1)
         return y
 
+    # Time complexity: O(n); Amortized time: O(1) [when resize: a.length = 2n; n add/remove availables --> O(n)/n]
     def add(self, i, x):
         if i < 0 or i > self.n : raise IndexError
-        if self.n == len(self.a) : self.resize()
+        if self.n == len(self.a) : self.resize()  # Copy n elements: O(n)
         self.a[i + 1:self.n + 1] = self.a[i:self.n]
-        self.a[i] = x
+        self.a[i] = x  # O(1) + O(n) = O(n)
         self.n += 1
 
+    # ""
     def remove(self, i):
         self.error(i)
         x = self.a[i]
--- Data_Structures_Library/src/data_structures/sllist.py
+++ Data_Structures_Library/src/data_structures/sllist.py
@@ -13,6 +13,29 @@ class SLList:
     def new_node(x):
         return SLList.Node(x)
 
+    # Stack operations (LIFO)
+
+    def _remove(self):
+            return self.pop()
+    
+    def pop(self):
+        if self.n == 0: return None
+        x = self.head.x
+        self.head = self.head.next
+        self.n -= 1
+        if self.n == 0: 
+            self.tail = None
+        return x
+
+    def push(self, x):
+        node = self.new_node(x)
+        node.next = self.head
+        self.head = node
+        if self.n == 0:
+            self.tail = node
+        self.n += 1
+        return True
+        
     def _add(self, x):
         node = self.new_node(x)
         if self.n == 0:
