886742a3d235699d96e6036d71ea6d2d03d27852
Yasser <yelmansouri15@gmail.com>
2026-09-16 19:13:31 +0200

    SLList: Add & Remove added

--- Data_Structures_Library/src/data_structures/sllist.py
+++ Data_Structures_Library/src/data_structures/sllist.py
@@ -13,10 +13,7 @@ class SLList:
     def new_node(x):
         return SLList.Node(x)
 
-    # Stack operations (LIFO)
-
-    def _remove(self):
-            return self.pop()
+    # Stack operations (LIFO); time complexity: O(1)
     
     def pop(self):
         if self.n == 0: return None
@@ -35,16 +32,6 @@ class SLList:
             self.tail = node
         self.n += 1
         return True
-        
-    def _add(self, x):
-        node = self.new_node(x)
-        if self.n == 0:
-            self.head = node
-        else:
-            self.tail.next = node
-        self.tail = node
-        self.n += 1
-        return True
 
 # Queue operations (FIFO); time complexity: O(1)
 
@@ -60,8 +47,9 @@ class SLList:
         self.tail = node
         self.n += 1
         return True
-    #
+#
 
+    # Time complexity: O(n)
     def get(self, i):
         if i < 0 or i >= self.n : raise IndexError
 
@@ -71,8 +59,41 @@ class SLList:
 
         return node
 
+    # Time complexity: O(n)
     def set(self, i, x):
         node = self.get(i)
         y = node.x
         node.x = x
         return y
+
+    # Time complexity: O(n)
+    def add(self, i, x):
+        if i < 0 or i >= self.n : raise IndexError
+        if i == 0: 
+            self.push(x)
+            return True
+
+        u = self.head
+        for _ in range(i-1):
+            u = u.next
+
+        node = self.new_node(x)
+        node.next = u.next
+        u.next = node
+        self.n += 1
+        return True
+
+    # Time complexity: O(n)
+    def remove(self, i):
+        if i < 0 or i >= self.n : raise IndexError
+        if i == 0: return self.pop()
+
+        u = self.head
+        for _ in range(i-1):
+            u = u.next     
+
+        x = u.next.x
+        u.next = u.next.next   
+        self.n -= 1
+        return x
+    
