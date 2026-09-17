82b0f66ac83901f5ee46d1ff4edc9e55158e4278
Yasser <yelmansouri15@gmail.com>
2026-09-17 19:51:12 +0200

    DLList: Get & Set added

--- Data_Structures_Library/src/data_structures/dllist.py
+++ Data_Structures_Library/src/data_structures/dllist.py
@@ -22,3 +22,22 @@ class DLList:
     def error(self, i):
         if i < 0 or i >= self.n : raise IndexError
 
+    def get(self, i):
+        self.error(i)
+
+        if i < self.n/2:
+            node = self.dummy.next
+            for _ in range (i):
+                node = node.next
+
+        node = self.dummy
+        for _ in range (self.n,i,-1):
+            node = node.prev
+
+        return node
+
+    def set(self, i, x):
+        node = self.get(i)
+        y = node.x
+        node.x = x
+        return y
