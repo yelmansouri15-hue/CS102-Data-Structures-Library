b6349e09ebf2c67e19117bb590da4925c7885bb9
Yasser <yelmansouri15@gmail.com>
2026-09-17 21:11:13 +0200

    Previous tests: improvement of error handling

--- Data_Structures_Library/src/tests/test_sllist.py
+++ Data_Structures_Library/src/tests/test_sllist.py
@@ -34,73 +34,60 @@ while True:
         if election is not False and election >= first_option and election <= last_option:
             break
 
-    match election:
-        case 0:
-            break
+    try:
+        match election:
+            case 0:
+                break
 
-        case 1:
-            try:
-                index = choose_index()
+            case 1:
+                    index = choose_index()
 
-                element_get = test.get(index)
+                    element_get = test.get(index)
 
-                print(f"The element of the index number {index} is {element_get}")
+                    print(f"The element of the index number {index} is {element_get}")
+                    show_array()
+
+            case 2:
+                    index = choose_index()
+                    element = input("Choose an element to set: ")
+
+                    element_set = test.set(index, element)
+
+                    print(f"{element} set in the index number {index}, replacing {element_set}")
+                    show_array()
+
+            case 3:
+                    index = choose_index()
+                    element = input("Choose an element to add: ")
+
+                    test.add(index, element)
+
+                    print(f"{element} added in the index number {index}")
+                    show_array()                    
+
+            case 4:
+                    index = choose_index()
+
+                    element_remove = test.remove(index)
+
+                    print(f"{element_remove} removed form the index number {index}")
+                    show_array()                    
+
+            case 5:
+                element = input("Choose an value to add at the head: ")
+
+                test.push(element)
+
+                print(f"A node with the values {element} is added at the head")
                 show_array()
 
-            except IndexError:
-                print("Index out of bounds")
+            case 6:
+                element_pop = test.pop()
 
-        case 2:
-            try:
-                index = choose_index()
-                element = input("Choose an element to set: ")
-
-                element_set = test.set(index, element)
-
-                print(f"{element} set in the index number {index}, replacing {element_set}")
+                print(f"The head, with a value of {element_pop}, was removed")
                 show_array()
 
-            except IndexError:
-                print("Index out of bounds")
-
-        case 3:
-            try:
-                index = choose_index()
-                element = input("Choose an element to add: ")
-
-                test.add(index, element)
-
-                print(f"{element} added in the index number {index}")
-                show_array() 
-
-            except IndexError:
-                print("Index out of bounds")                   
-
-        case 4:
-            try:
-                index = choose_index()
-
-                element_remove = test.remove(index)
-
-                print(f"{element_remove} removed form the index number {index}")
+            case 7:
                 show_array()
-                     
-            except IndexError:
-                print("Index out of bounds")
-
-        case 5:
-            element = input("Choose an value to add at the head: ")
-
-            test.push(element)
-
-            print(f"A node with the values {element} is added at the head")
-            show_array()
-
-        case 6:
-            element_pop = test.pop()
-
-            print(f"The head, with a value of {element_pop}, was removed")
-            show_array()
-
-        case 7:
-            show_array()
+    except IndexError:
+        print("Index out of bounds")
